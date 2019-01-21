375063d0bb457e8b4aaee5606a1372debfc494fe
unknown <dbrudne1@gmail.com>
2019-01-21 10:10:27 -0600

    adding teams and conference standings endpoints

---
+++ conf_standings.go
@@ -0,0 +1,61 @@
+package main
+
+// Structs for endpoint to retrieve conference standings
+// src: http://data.nba.net/prod/v1/current/standings_conference.json
+// date accessed: 2019-01-21 10:03:41.77
+
+type ConfStandings struct {
+	Internal Internal `json:"_internal"`
+	League   League   `json:"league"`
+}
+
+type Internal struct {
+	PubDateTime string `json:"pubDateTime"`
+	XSLT        string `json:"xslt"`
+	EventName   string `json:"eventName"`
+}
+
+type League struct {
+	Standard Standard `json:"standard"`
+}
+
+type Standard struct {
+	SeasonYear    int64      `json:"seasonYear"`
+	SeasonStageID int64      `json:"seasonStageId"`
+	Conference    Conference `json:"conference"`
+}
+
+type Conferences struct {
+	East []Conference `json:"east"`
+	West []Conference `json:"west"`
+}
+
+type Conference struct {
+	TeamID                 string           `json:"teamId"`
+	Win                    string           `json:"win"`
+	Loss                   string           `json:"loss"`
+	WinPct                 string           `json:"winPct"`
+	WinPctV2               string           `json:"winPctV2"`
+	LossPct                string           `json:"lossPct"`
+	LossPctV2              string           `json:"lossPctV2"`
+	GamesBehind            string           `json:"gamesBehind"`
+	DivGamesBehind         string           `json:"divGamesBehind"`
+	ClinchedPlayoffsCode   string           `json:"clinchedPlayoffsCode"`
+	ClinchedPlayoffsCodeV2 string           `json:"clinchedPlayoffsCodeV2"`
+	ConfRank               string           `json:"confRank"`
+	ConfWin                string           `json:"confWin"`
+	ConfLoss               string           `json:"confLoss"`
+	DivWin                 string           `json:"divWin"`
+	DivLoss                string           `json:"divLoss"`
+	HomeWin                string           `json:"homeWin"`
+	HomeLoss               string           `json:"homeLoss"`
+	AwayWin                string           `json:"awayWin"`
+	AwayLoss               string           `json:"awayLoss"`
+	LastTenWin             string           `json:"lastTenWin"`
+	LastTenLoss            string           `json:"lastTenLoss"`
+	Streak                 string           `json:"streak"`
+	DivRank                string           `json:"divRank"`
+	IsWinStreak            bool             `json:"isWinStreak"`
+	TieBreakerPts          string           `json:"tieBreakerPts"`
+	SortKey                map[string]int64 `json:"sortKey"`
+}
---
+++ teams.go
@@ -0,0 +1,59 @@
+package main
+
+// Structs for endpoint to retrieve teams
+// src: http://data.nba.net/prod/v2/2018/teams.json
+// date accessed: 2018-08-30 20:00:04.422
+
+type Teams struct {
+	Internal struct {
+		PubDateTime string `json:"pubDateTime"`
+		XSLT        string `json:"xslt"`
+		EventName   string `json:"eventName"`
+	}
+
+	League struct {
+		Standard   []Africa `json:"standard"`
+		Africa     []Africa `json:"africa"`
+		Sacramento []Africa `json:"sacramento"`
+		Vegas      []Africa `json:"vegas"`
+		Utah       []Africa `json:"utah"`
+	}
+}
+
+type Africa struct {
+	IsNBAFranchise bool         `json:"isNBAFranchise"`
+	IsAllStar      bool         `json:"isAllStar"`
+	City           string       `json:"city"`
+	AltCityName    string       `json:"altCityName"`
+	FullName       string       `json:"fullName"`
+	Tricode        string       `json:"tricode"`
+	TeamID         string       `json:"teamId"`
+	Nickname       string       `json:"nickname"`
+	URLName        string       `json:"urlName"`
+	ConfName       ConfNameEnum `json:"confName"`
+	DivName        DivNameEnum  `json:"divName"`
+}
+
+type ConfNameEnum string
+
+const (
+	ConfName   ConfNameEnum = ""
+	East       ConfNameEnum = "East"
+	Intl       ConfNameEnum = "Intl"
+	Sacramento ConfNameEnum = "Sacramento"
+	Summer     ConfNameEnum = "summer"
+	Utah       ConfNameEnum = "Utah"
+	West       ConfNameEnum = "West"
+)
+
+type DivNameEnum string
+
+const (
+	Atlantic  DivNameEnum = "Atlantic"
+	Central   DivNameEnum = "Central"
+	DivName   DivNameEnum = ""
+	Northwest DivNameEnum = "Northwest"
+	Pacific   DivNameEnum = "Pacific"
+	Southeast DivNameEnum = "Southeast"
+	Southwest DivNameEnum = "Southwest"
+)
