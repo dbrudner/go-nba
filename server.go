860234db437c50aa693e3c47f1d8d36cee51cc7d
unknown <dbrudne1@gmail.com>
2019-01-31 19:08:53 -0600

    setting teams array to key Teams in json in getTeams endpoint

--- server.go
+++ server.go
@@ -57,10 +57,16 @@ func getPlayerStats(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(player)
 }
 
+type GetTeamsResponse struct {
+	Teams []nba.Teams_Team
+}
+
 func getTeams(w http.ResponseWriter, r *http.Request) {
 	endpoints := nba.FetchTodayInfo()
 	teams := nba.FetchAllNBATeams(endpoints.Links.Teams)
+	response := GetTeamsResponse{Teams: teams}
+	fmt.Printf("%T", response)
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(teams)
+	json.NewEncoder(w).Encode(response)
 }
